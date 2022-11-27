279b86664854888a2fe03fbfa57257060ea8bb1e
weg <wendyelviragarcia@gmail.com>
2022-11-27 21:43:06 +0100

    update encoding

--- server.R
+++ server.R
@@ -10,11 +10,12 @@ library("dplyr")
 library("gridExtra")
 library("factoextra")
 library("FactoMineR")
+library("tidyverse")
+library("readtextgrid")
 
+#options(shiny.maxRequestSize=30*1024^2) 
 
-options(shiny.maxRequestSize=30*1024^2) 
-
-server <- function(input, output) {
+server <- function(input, output){
   #options(shiny.maxRequestSize=30*1024^2) 
   output$contents <- renderTable({
     
@@ -23,6 +24,9 @@ server <- function(input, output) {
     # or all rows if selected, will be shown.
     
     req(input$file1)
+    #req(input$annotation)
+  #req(input$tier)
+    
     
     # when reading semicolon separated files,
     # having a comma separator causes `read.csv` to error
@@ -41,43 +45,46 @@ server <- function(input, output) {
           #for (file in files) {
           #loopIndex=loopIndex+1
           #textgrid<- read_textgrid(file)
-          #textgrid= readTextGridRobust("/Users/weg/Desktop/Alpasarlabarca.TextGrid", "UTF-8")[[2]]
+          #textgrid= readTextGridRobust("/Users/weg/Desktop/untitled.TextGrid", "UTF-16")[[2]]
           #textgrid<- read_textgrid(files[loopIndex])
           filetoRead <- files[loopIndex]
           #print(filetoRead)
           textgrid <- tryCatch(
             expr = {
-              textgrid <- readTextGridRobust(filetoRead, "UTF-16")[[2]]
-            },
-            error = function(e){
-              # (Optional)
-              # Do this if an error is caught...
-              message("No se puede leer el fichero TextGrid:")
-              message(e)
-              # Choose a return value in case of error
+              #print({input$tier})
               
-              textgrid <- readTextGridRobust(filetoRead, "UTF-8")[[2]]
-              return(textgrid)
+              textgrid <- readTextGridRobust(filetoRead, "UTF-16BE")[[{input$tier}+1]]
             },
             warning = function(w){
               # (Optional)
-              # Do this if an warning is caught...
-              message("Error de lectura, probando UTF-8:")
-              message(w)
-              textgrid <- readTextGridRobust(filetoRead, "UTF-8")[[2]]
+              # Do this if an error is caught...
+              message("Error de lectura, buscando codificación de manera automática...")
+              # Choose a return value in case of error
+              encoding = guess_encoding(filetoRead) %>% filter(confidence == 1) %>% pluck("encoding")
+              textgrid <- readTextGridRobust(filetoRead, encoding)[[{input$tier}+1]]
               return(textgrid)
             }
           )
           
           
-          
-          
-          
           fileName<- input$file1$name[loopIndex]
-          textgrid$Outcomes[textgrid$Outcomes=="PTK"] <- "C"
-          textgrid$Outcomes[textgrid$Outcomes=="a"] <- "V"
+          
+          if (input$annotation == 1){
+            textgrid$Outcomes <- gsub( "[ ˈˈ̬̞̝̪̟̠̥̃]", "", textgrid$Outcomes)
+            textgrid$Outcomes <- gsub( "[^aeoiujwaəεɑɔ]", "C", textgrid$Outcomes)
+            textgrid$Outcomes <- gsub( "[aeoiujwəεɑɔ]", "V", textgrid$Outcomes)
+           } else if (input$annotation == 2){
+             textgrid$Outcomes <- gsub( "[h´,.;:]", "", textgrid$Outcomes)
+             textgrid$Outcomes <- gsub( "[^aeiou]", "C", textgrid$Outcomes)
+             textgrid$Outcomes <- gsub( "[aeiou]", "V", textgrid$Outcomes)
+           } else if (input$annotation == 3){
+             textgrid$Outcomes[textgrid$Outcomes=="PTK"] <- "C"
+             textgrid$Outcomes[textgrid$Outcomes=="a"] <- "V"
+           }
+          
           textgrid$Outcomes[textgrid$Outcomes=="c"] <- "C"
           textgrid$Outcomes[textgrid$Outcomes=="v"] <- "V"
+          
           #computes dur in ms for comparison purposes with other metrics (Arvaniti 2011)
           textgrid$duration<-(textgrid$end-textgrid$start)*1000
           textGridNonSilent<-textgrid[textgrid$Outcomes=="C"|textgrid$Outcomes=="V",]
@@ -97,6 +104,10 @@ server <- function(input, output) {
           nCons =unname(frequ[names(frequ)=="C"])
           nVows =unname(frequ[names(frequ)=="V"])
           speechRate= (nCons+nVows)/speechTime
+          if (identical(speechRate, numeric(0))){
+            speechRate = 0
+          }
+          
           PercentageV = (vowelTime/speechTime)*100
           PercentageC = (consonantTime/speechTime)*100
           deltaC <- sd(textgrid[textgrid$Outcomes=="C",]$duration)
@@ -177,142 +188,143 @@ server <- function(input, output) {
         
         datos= data.frame( df[,2:11], row.names =df$file )
         
-        res.pca <- PCA(datos, ncp = 3, graph = FALSE)
-        maxClus=length(res.pca$eig)
+        #res.pca <- PCA(datos, ncp = 3, graph = FALSE)
+        #maxClus=length(res.pca$eig)
         
-        if (nFiles>2){
-          res.hcpc <- HCPC(res.pca, max=maxClus, graph = FALSE)
+        #if (nFiles>2){
+          #res.hcpc <- HCPC(res.pca, max=maxClus, graph = FALSE)
           
-          output$plot6 <- renderPlot({
-            fviz_dend(res.hcpc, 
-                      cex = 0.7,                     # Label size
-                      palette = "jco",               # Color palette see ?ggpubr::ggpar
-                      rect = TRUE, rect_fill = TRUE, # Add rectangle around groups
-                      rect_border = "jco",           # Rectangle color
-                      labels_track_height = 0.8      # Augment the room for labels
-            )
-          })
-          
-          
-          output$plot7 <- renderPlot({
-            fviz_cluster(res.hcpc,
-                         repel = TRUE,            # Avoid label overlapping
-                         show.clust.cent = TRUE, # Show cluster centers
-                         palette = "jco",         # Color palette see ?ggpubr::ggpar
-                         ggtheme = theme_minimal(),
-                         main = "Factor map"
-            )
-          })
+          #output$plot6 <- renderPlot({
+          #  fviz_dend(res.hcpc, 
+          #            cex = 0.7,                     # Label size
+          #            palette = "jco",               # Color palette see ?ggpubr::ggpar
+          #           rect = TRUE, rect_fill = TRUE, # Add rectangle around groups
+          #            rect_border = "jco",           # Rectangle color
+          #            labels_track_height = 0.8      # Augment the room for labels
+          #  )
+          # })
           
           
+        # output$plot7 <- renderPlot({
+        #   fviz_cluster(res.hcpc,
+        #                repel = TRUE,            # Avoid label overlapping
+        #                show.clust.cent = TRUE, # Show cluster centers
+        #                palette = "jco",         # Color palette see ?ggpubr::ggpar
+        #               ggtheme = theme_minimal(),
+        #               main = "Factor map"
+        #   )
+        #  })
+        
           
           
-        }
         
         
-        df.summary.PV <- df %>%group_by(file) %>%
-          summarize(ymin = min(PerV),
-                    ymax = max(PerV),
-                    ymean = mean(PerV))
         
-        df.summary.VV <- df %>% group_by(file) %>%
-          summarize(ymin = min(VarcoV),
-                    ymax = max(VarcoV),
-                    ymean = mean(VarcoV))
+        # df.summary.PV <- df %>%group_by(file) %>%
+        #   summarize(ymin = min(PerV),
+        #             ymax = max(PerV),
+        #             ymean = mean(PerV))
+        # 
+        # df.summary.VV <- df %>% group_by(file) %>%
+        #   summarize(ymin = min(VarcoV),
+        #             ymax = max(VarcoV),
+        #             ymean = mean(VarcoV))
+        # 
+        # df.summary.DC <- df %>% group_by(file) %>%
+        #   summarize(ymin = min(DeltaC),
+        #             ymax = max(DeltaC),
+        #             ymean = mean(DeltaC))
+        # 
+        # df.summary.DV <- df %>% group_by(file) %>%
+        #   summarize(ymin = min(DeltaV),
+        #             ymax = max(DeltaV),
+        #             ymean = mean(DeltaV))
+        # 
+        # df.summary.VnPVI <- df %>% group_by(file) %>%
+        #   summarize(ymin = min(VnPVI),
+        #             ymax = max(VnPVI),
+        #             ymean = mean(VnPVI))
+        # 
+        # df.summary.CrPVI <- df %>% group_by(file) %>%
+        #   summarize(ymin = min(CrPVI),
+        #             ymax = max(CrPVI),
+        #             ymean = mean(CrPVI))
         
-        df.summary.DC <- df %>% group_by(file) %>%
-          summarize(ymin = min(DeltaC),
-                    ymax = max(DeltaC),
-                    ymean = mean(DeltaC))
-        
-        df.summary.DV <- df %>% group_by(file) %>%
-          summarize(ymin = min(DeltaV),
-                    ymax = max(DeltaV),
-                    ymean = mean(DeltaV))
-        
-        df.summary.VnPVI <- df %>% group_by(file) %>%
-          summarize(ymin = min(VnPVI),
-                    ymax = max(VnPVI),
-                    ymean = mean(VnPVI))
-        
-        df.summary.CrPVI <- df %>% group_by(file) %>%
-          summarize(ymin = min(CrPVI),
-                    ymax = max(CrPVI),
-                    ymean = mean(CrPVI))
-        
-        names(allC)[1] <- "file"
-        names(allV)[1] <- "file"
-        allC$file <- as.character(allC$file)
-        allV$file <- as.character(allV$file)
+         names(allC)[1] <- "file"
+         names(allV)[1] <- "file"
+         allC$file <- as.character(allC$file)
+         allV$file <- as.character(allV$file)
         
         allC$file <- substr(allC$file,1,nchar(allC$file)-9)
         allV$file <- substr(allV$file,1,nchar(allV$file)-9)
         
-        allC$file <- as.factor(allC$file)
-        allV$file <- as.factor(allV$file)
+         allC$file <- as.factor(allC$file)
+         allV$file <- as.factor(allV$file)
+      
         
-        p1<- ggplot(allC, aes(x=file, y=durCs,colour = file)) +
-          geom_boxplot()+ylab("(ms)")+xlab("File")+
-          theme(axis.text.x = element_text(angle = 90))+
-          ggtitle("Consonants Duration")
-        
-        p2<- ggplot(allV, aes(x=file, y=durVs,colour = file)) +
-          geom_boxplot()+ylab("(ms)")+xlab("File")+
-          theme(axis.text.x = element_text(angle = 90))+
-          ggtitle("Vowels Duration")
-        
-        output$plot1 <- renderPlot({
-          grid.arrange(p1, p2, ncol=2)
+          p1<- ggplot(allC, aes(x=file, y=durCs, colour=file)) +
+            geom_boxplot()+ylab("(ms)")+xlab("File")+
+            theme(legend.position = "none")+
+            #theme(axis.text.x = element_text(angle = 90))+
+            ggtitle("Consonants Duration")
           
-        }) 
-        
-        
-        
-        output$plot5 <- renderPlot({
-          ggplot(data = df,aes(x = CrPVI, y = VnPVI, colour = file)) +
-            geom_point()+
-            labs(subtitle="Consonantal raw PVI by Vocalic normalized PVI", 
-                 y="VnPVI", 
-                 x="CrPVI", 
-                 title="CrPVI by VnPVI", 
-                 caption = "Metric Grabe & Low (2002)")
-        }) 
-        
-        output$plot2 <- renderPlot({
-          ggplot(data = df,aes(x = VarcoV, y = PerV, colour = file)) +
-            geom_point()+
-            #geom_errorbar(aes(ymin = df.summary.VV$ymin, ymax = df.summary.VV$ymax))+
-            #geom_errorbarh(aes(xmin = df.summary.PV$ymin,xmax = df.summary.PV$ymax))+
-            labs(subtitle="%V/varcoV",
-                 y="%V", 
-                 x="VarcoV", 
-                 title="%V/varcoV by file", 
-                 caption = "GNU. Made with www.wendyelvira.ga ")
-        }) 
-        
-        
-        
-        output$plot3 <- renderPlot({
-          ggplot(data = df,aes(x = PerV, y = DeltaC, colour = file)) +
-            geom_point()+
-            labs(subtitle="%V/∆C", 
-                 y="∆C", 
-                 x="%V", 
-                 title="%V/∆C", 
-                 caption = "GNU. Made with www.wendyelvira.ga ")
-        }) 
-        
-        
-        output$plot4 <- renderPlot({
-          ggplot(data = df,aes(x = DeltaV, y = DeltaC, colour = file)) +
-            geom_point()+
-            labs(subtitle="∆V/∆C", 
-                 y="∆C", 
-                 x="∆V",  
-                 title="∆V/∆C", 
-                 caption = "GNU. Made with www.wendyelvira.ga ")
-        }) 
-        
+          p2<- ggplot(allV, aes(x=file, y=durVs, colour=file)) +
+            geom_boxplot()+ylab("(ms)")+xlab("File")+
+            theme(legend.position = "none")+
+            #theme(axis.text.x = element_text(angle = 90))+
+            ggtitle("Vowels Duration")
+         
+         output$plot1 <- renderPlot({
+           grid.arrange(p1, p2, ncol=2)
+         
+         }) 
+        # 
+        # 
+        # 
+        # output$plot5 <- renderPlot({
+        #   ggplot(data = df,aes(x = CrPVI, y = VnPVI, colour = file)) +
+        #     geom_point()+
+        #     labs(subtitle="Consonantal raw PVI by Vocalic normalized PVI", 
+        #          y="VnPVI", 
+        #          x="CrPVI", 
+        #          title="CrPVI by VnPVI", 
+        #          caption = "Metric Grabe & Low (2002)")
+        # }) 
+        # 
+        # output$plot2 <- renderPlot({
+        #   ggplot(data = df,aes(x = VarcoV, y = PerV, colour = file)) +
+        #     geom_point()+
+        #     #geom_errorbar(aes(ymin = df.summary.VV$ymin, ymax = df.summary.VV$ymax))+
+        #     #geom_errorbarh(aes(xmin = df.summary.PV$ymin,xmax = df.summary.PV$ymax))+
+        #     labs(subtitle="%V/varcoV",
+        #          y="%V", 
+        #          x="VarcoV", 
+        #          title="%V/varcoV by file", 
+        #          caption = "GNU. Made with www.wendyelvira.ga ")
+        # }) 
+        # 
+        # 
+        # 
+        # output$plot3 <- renderPlot({
+        #   ggplot(data = df,aes(x = PerV, y = DeltaC, colour = file)) +
+        #     geom_point()+
+        #     labs(subtitle="%V/∆C", 
+        #          y="∆C", 
+        #          x="%V", 
+        #          title="%V/∆C", 
+        #          caption = "GNU. Made with www.wendyelvira.ga ")
+        # }) 
+        # 
+        # 
+        # output$plot4 <- renderPlot({
+        #   ggplot(data = df,aes(x = DeltaV, y = DeltaC, colour = file)) +
+        #     geom_point()+
+        #     labs(subtitle="∆V/∆C", 
+        #          y="∆C", 
+        #          x="∆V",  
+        #          title="∆V/∆C", 
+        #          caption = "GNU. Made with www.wendyelvira.ga ")
+        # }) 
         
         
         
--- ui.R
+++ ui.R
@@ -11,7 +11,6 @@ ui <- fluidPage(
     
     # Sidebar panel for inputs ----
     sidebarPanel(width = 3,
-      
       # Input: Select a file ----
       fileInput("file1", "Choose your TextGrids. They must have C and V labelled intervals",
                 multiple = TRUE),
@@ -19,37 +18,41 @@ ui <- fluidPage(
       # Horizontal line ----
       tags$hr(),
       
+      
+      # Input: select tier where segment annotation is
+      numericInput("tier", h4("N tier by-sound annotation"),3, 1,100,1),
+ 
+      # Input: select annotation type
+      selectInput("annotation", h4("Annotation type"), 
+                  choices = list("Phonetic" = 1,"Orthographic" = 2, "C and V" = 3,
+                                 "PTK" = 4), selected = 1),
+      
       # Input: Select number of rows to display ----
-      checkboxGroupInput("disp", "Metrics",
-                   choices = c(SpeechRate = "SpeechRate", PercentageConsonants = "PercentageConsonants",
-                               PercentageVowel = "PercentageVowel", VarcoV="Varco", VarcoC = "VarcoC"),
-                   selected = c("SpeechRate","PercentageVowel","PercentageConsonants")),
+      #checkboxGroupInput("disp", "Metrics",
+      #             choices = c(SpeechRate = "SpeechRate", PercentageConsonants = "PercentageConsonants",
+      #                        PercentageVowel = "PercentageVowel", VarcoV="Varco", VarcoC = "VarcoC"),
+      #            selected = c("SpeechRate","PercentageVowel","PercentageConsonants")),
       
       tags$hr(),
       # Input: Select number of rows to display ----
-      checkboxGroupInput("disp", "Graphs",
-                         choices = c(PerV = "PerV", Varcos = "Varcos",
-                                     Deltas = "Deltas"),
-                         selected = c("PerV","Varcos","PercentageConsonants")),
+      # checkboxGroupInput("disp", "Graphs",
+      #                  choices = c(PerV = "PerV", Varcos = "Varcos",
+      #                               Deltas = "Deltas"),
+      #                   selected = c("PerV","Varcos","PercentageConsonants")),
       
     ),
     
     # Main panel for displaying outputs ----
     mainPanel(
-      
       # Output: Data file ----
       h4("Numeric Results"),
       tableOutput("contents"),
       h4("Graphs"),
       plotOutput("plot1"),
-      plotOutput("plot2"),
-      plotOutput("plot3"),
-      plotOutput("plot4"),
-      plotOutput("plot5"),
-      plotOutput("plot6"),
-      plotOutput("plot7")
-      
-      
+      plotOutput("plot2")
+      #plotOutput("plot3"),
+      #plotOutput("plot4"),
+     # plotOutput("plot5")
       
     )
     
